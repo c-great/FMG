07c21c4b1392a21f58e1096a168865117918a64a
Nick McKubre <nick@mckubre.com>
2020-08-20 17:03:19 -0400

    started creating bare-bones UI

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:fmg_remote_work_tracker/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(FmgApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
