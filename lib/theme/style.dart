a31d6d52daf917ac0c56f5c20e640d8e57e8f1a8
Nick McKubre <nick@mckubre.com>
2020-09-15 12:05:06 -0400

    updated style to match FMG colors

--- lib/theme/style.dart
+++ lib/theme/style.dart
@@ -2,9 +2,10 @@ import 'package:flutter/material.dart';
 
 ThemeData appTheme() {
   return ThemeData(
-    // made green because it is FMG's colour. But I haven't tried to match
-    // the exact shade yet.
-    primarySwatch: Colors.green,
+    // matched FMG color for primary
+    primaryColor: Color(0xFF006938),
+    primaryColorLight: Color(0xFF419863),
+    primaryColorDark: Color(0xFF003d11),
     visualDensity: VisualDensity.adaptivePlatformDensity,
   );
 }
