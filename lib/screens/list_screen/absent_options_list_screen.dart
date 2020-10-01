80cd0dc0f10f73a51de298249f7a6806a82a872b
Nick McKubre <nick@mckubre.com>
2020-10-01 16:36:05 -0400

    Other additional info can now be entered on select screen

--- lib/screens/list_screen/absent_options_list_screen.dart
+++ lib/screens/list_screen/absent_options_list_screen.dart
@@ -1,18 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:fmg_remote_work_tracker/models/employee_location.dart';
 import 'package:fmg_remote_work_tracker/screens/list_screen/list_entry.dart';
+import 'package:fmg_remote_work_tracker/screens/list_screen/other_dialog.dart';
 import 'package:fmg_remote_work_tracker/screens/list_screen/select_from_list_screen.dart';
 
 List<Widget> getAbsentOptionsList(BuildContext context) {
   List<Widget> widgets = new List();
   AbsentOptions.values.forEach((element) {
-    var tile = getRegularListItem(
-        context,
-        element.asString(),
-        EmployeeAbsent(absenceType: element));
-    widgets.add(tile);
+    // ignore other because it is treated specially
+    if (element != AbsentOptions.OTHER) {
+      var tile = getRegularListItem(
+          context,
+          element.asString(),
+          EmployeeAbsent(absenceType: element));
+      widgets.add(tile);
+    }
   });
+  // add other option
+  widgets.add(Divider());
+  var otherTile = getOtherListItem(otherAbsentDialog(context));
+  widgets.add(otherTile);
   return widgets;
+}
+
+Function otherAbsentDialog(BuildContext context) {
+  return () {
+    showDialog<EmployeeLocation>(
+        context: context,
+        builder: (BuildContext context) {
+          return createOtherAdditionalInfoDialog(context, (additionalInfo) {
+            return EmployeeAbsent(
+                absenceType: AbsentOptions.OTHER,
+                additionalInfo: additionalInfo);
+          },
+          "Enter the Type of Absence",
+          "e.g. Bereavement");
+        }
+    );
+  };
 }
 
 class AbsenceTypeListScreen extends SelectFromListScreen {
