1941d7b80be289a7c0c4600d30d305417ae03186
Nick McKubre <nick@mckubre.com>
2020-10-08 15:11:59 -0400

    looking into data push notifications for android (very experimental!)

--- lib/server_interaction/push_notifications.dart
+++ lib/server_interaction/push_notifications.dart
@@ -2,6 +2,20 @@
 
 import 'package:firebase_messaging/firebase_messaging.dart';
 
+Future<dynamic> myBackgroundMessageHandler(Map<String, dynamic> message) async {
+  if (message.containsKey('data')) {
+    // Handle data message
+    final dynamic data = message['data'];
+  }
+
+  if (message.containsKey('notification')) {
+    // Handle notification message
+    final dynamic notification = message['notification'];
+  }
+
+  // Or do other work.
+}
+
 class PushNotificationsManager {
 
   PushNotificationsManager._();
@@ -17,7 +31,18 @@ class PushNotificationsManager {
     if (!_initialized) {
       // For iOS request permission first.
       _firebaseMessaging.requestNotificationPermissions();
-      _firebaseMessaging.configure();
+      _firebaseMessaging.configure(
+        onMessage: (Map<String, dynamic> message) async {
+          print("onMessage: $message");
+        },
+        onBackgroundMessage: myBackgroundMessageHandler,
+        onLaunch: (Map<String, dynamic> message) async {
+          print("onLaunch: $message");
+        },
+        onResume: (Map<String, dynamic> message) async {
+          print("onResume: $message");
+        },
+      );
 
       _initialized = true;
     }
