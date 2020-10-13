a4374bfd0c14ca3092f13ec3fce90a6978594e1d
Reuben Bidlake <reuben1@hotmail.co.nz>
2020-10-14 00:41:28 +1300

    Now uses information from the database

--- lib/screens/team_screen/employee_cards.dart
+++ lib/screens/team_screen/employee_cards.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:fmg_remote_work_tracker/models/employee.dart';
 import 'package:fmg_remote_work_tracker/models/employee_location.dart';
 import 'package:fmg_remote_work_tracker/screens/schedule_future_screen/future_location_dialog.dart';
+import 'package:fmg_remote_work_tracker/server_interaction/basic_interaction.dart';
 
 List<Widget> createEmployeeCards(BuildContext context, List<Employee> reportingEmployees,
     Map<String, EmployeeLocation> reportingEmployeeLocations) {
   List<Widget> widgetList = [];
+  EmployeeLocation currentLocation = new EmployeeLocation();
 
   reportingEmployees.forEach((employee) {
     Card employeeCard = new Card(
@@ -17,10 +19,13 @@ List<Widget> createEmployeeCards(BuildContext context, List<Employee> reportingE
             trailing: IconButton(
               icon: Icon(Icons.edit),
               // edit location
-              onPressed: () {
+              onPressed: () async {
+                currentLocation = await getLocationFromID(employee.employeeID);
                 showDialog(context: context,
                 builder: (BuildContext context) {
-                  return editLocationDialog(context, EmployeeAbsent(absenceType: AbsentOptions.SICK_LEAVE), EmployeeAtOffice(officeLocation: "something"), EmployeeAbsent(absenceType: AbsentOptions.SICK_LEAVE));
+                  return editLocationDialog(context, employee.employeeID, EmployeeAbsent(absenceType: AbsentOptions.ANNUAL_LEAVE),
+                      EmployeeAtOffice(officeLocation: "Wellington"),
+                      currentLocation);
                 },);
               },),
           ),
